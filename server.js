2c10b4874319156e2ebcd40a896d3b8dde5d4897
amrit123456789 <amritagarwal98@gmail.com>
2019-01-05 13:47:33 +0530

    first commit
    Signed-off-by: amrit123456789 <amritagarwal98@gmail.com>

---
+++ server.js
@@ -0,0 +1,9 @@
+const express =require('express')
+const path =require('path')
+const app= express()
+
+app.use('/public' ,express.static(path.join(__dirname,'public')))
+
+app.get('/' , (req,res)=>{res.send('hello from backend')})
+
+app.listen(3333, ()=>{console.log("server started on https://localhost:3333")})
