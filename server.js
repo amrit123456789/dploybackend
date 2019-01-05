5cf8a70538147c63f351aec5bfb533eb639ab11d
amrit123456789 <amritagarwal98@gmail.com>
2019-01-05 14:08:02 +0530

    using port from env
    Signed-off-by: amrit123456789 <amritagarwal98@gmail.com>

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express =require('express')
 const path =require('path')
 const app= express()
 
+const SERVER_PORT= process.env.PORT || 3333
+
 app.use('/public' ,express.static(path.join(__dirname,'public')))
 
 app.get('/' , (req,res)=>{res.send('hello from backend')})
 
-app.listen(3333, ()=>{console.log("server started on https://localhost:3333")})
+app.listen(SERVER_PORT, ()=>{console.log("server started on https://localhost:3333")})
